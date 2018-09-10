df014dfd963cc8ddf6a5f2515c26a001305b223f
Eetu Blomqvist <eetu.blomqvist@reaktor.com>
2018-09-09 23:14:18 -0400

    Initial commit.

---
+++ app/src/main/java/fi/blomqvist/whentoleave/MainActivity.kt
@@ -0,0 +1,12 @@
+package fi.blomqvist.whentoleave
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
